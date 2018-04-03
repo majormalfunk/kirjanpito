0627316eb58ecd536074cd793617d36b951f52b1
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-04-04 00:57:51 +0300

    korjattu login-toiminnallisuutta

--- application/accounts/views.py
+++ application/accounts/views.py
@@ -1,4 +1,5 @@
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
 
 from application import app, db
 from application.accounts.models import Account
