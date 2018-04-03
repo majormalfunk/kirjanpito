fef6bbfdfdde2a054e87096c9e3a2ed0739bc2fc
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-04-04 00:25:24 +0300

    Lisätty kirjautumistoiminnallisuus

--- application/__init__.py
+++ application/__init__.py
@@ -22,5 +22,23 @@ from application import views
 from application.accounts import models
 from application.accounts import views
 
+# Kirjautuminen
+from application.auth import models
+from application.auth import views
+from application.auth.models import UserAccount
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.setup_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Sinun pitää kirjautua voidaksesi käyttää toiminnallisuutta"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserAccount.query.get(user_id)
+
 # Luodaan lopulta tarvittavat tietokantataulut
 db.create_all()
---
+++ application/auth/views.py
@@ -0,0 +1,29 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user
+
+from application import app
+from application.auth.models import UserAccount
+from application.auth.forms import LoginForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/loginform.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # mahdolliset validoinnit
+
+    user = UserAccount.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form = form,
+                               error = "Väärä käyttäjätunnus tai salasana")
+
+
+    print("Käyttäjä " + user.name + " tunnistettiin")
+    login_user(user)
+    return redirect(url_for("index"))
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index")) 
