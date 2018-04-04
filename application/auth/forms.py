90df6b411dee4aa3e89d8cb16a579bf37d52d8a6
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2018-04-04 22:50:45 +0300

    Rekisteröityminen toteutettu

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField
+from wtforms import validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("K&auml;tt&auml;j&auml;tunnus")
+    password = PasswordField("Salasana")
+  
+    class Meta:
+        csrf = False
+
+class RegisterForm(FlaskForm):
+    username = StringField("K&auml;ytt&auml;j&auml;tunnus", [
+        validators.Length(min=8, max=20, message="Pituus 8..20")])
+    password = PasswordField("Salasana", [
+        validators.Required(),
+        validators.Length(min=8, max=20, message="Pituus 8..20")])
+    confirm = PasswordField("Salasana uudestaan", [
+        validators.EqualTo("password", message="Salasanan pit&auml;&auml t&auml;sm&auml;t&auml;")])
+    name = StringField("Nimi", [
+        validators.Length(min=4, max=100, message="Pituus 4..100")])
   
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,10 +1,55 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import UserAccount
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
+#
+# Rekisteröityminen
+#
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form = RegisterForm())
+
+    form = RegisterForm(request.form)
+    # mahdolliset validoinnit
+    
+    print("Yritetään rekisteröitymistä")
+    if not form.validate():
+        return render_template("auth/registerform.html", form = form)
+
+    testuser = UserAccount.query.filter_by(username=form.username.data).first()
+    if testuser:
+        varattu = form.username.data
+        form.username.data = ""
+        return render_template("auth/registerform.html", form = form,
+                               error = "Käyttäjätunnus " + varattu + " on jo käytössä")
+
+    newuser = UserAccount(username=form.username.data, password=form.password.data, name=form.name.data)
+    db.session().add(newuser)
+    db.session().commit()
+    print("Käyttäjä " + newuser.name + " rekisteröitiin")
+
+    user = UserAccount.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/registerform.html", form = form,
+                               error = "Jokin meni pieleen rekisteröitymisessä")
+
+    login_user(user)
+    return redirect(url_for("index"))
+
+
+    ##loginForm = LoginForm()
+    ##loginForm.username = newuser.username
+    ##loginForm.password = newuser.password
+
+    ##return redirect(url_for("accounts_index", form = loginForm))
+
+##
+## Kirjautuminen
+##
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
     if request.method == "GET":
@@ -16,13 +61,16 @@ def auth_login():
     user = UserAccount.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                               error = "Väärä käyttäjätunnus tai salasana")
+                               error = "V&auml;&auml;r&auml; k&auml;ytt&auml;j&auml;tunnus tai salasana")
 
 
     print("Käyttäjä " + user.name + " tunnistettiin")
     login_user(user)
     return redirect(url_for("index"))
 
+##
+## Uloskirjautuminen
+##
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
